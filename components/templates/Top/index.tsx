e308ff642a169c7853a99807357487dee2125584
fukada_marietta <fukada.y@marietta.co.jp>
2022-03-30 15:27:40 +0900

    StyledComponentの追加

--- components/templates/Top/index.tsx
+++ components/templates/Top/index.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
 type Props = {
-  
+
 }
 
-export const Top = () => {};
+export const Top = () => {
+  return(<Div>TOP</Div>)
+};
+
+const Div = styled.div`
+  margin: 0;
+`
 
 
